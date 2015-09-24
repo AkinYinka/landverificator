f091c30d9b02c4bb3910fe4a91e197274ed5acc0
Live session user <ubuntu@ubuntu.(none)>
2015-09-24 18:21:54 +0000

    Add land owner verification software

---
+++ java import.util.java
@@ -0,0 +1,34 @@
+java import.util.scanner
+
+	public class LandVerificator (){
+		public static Scanner in = new Scanner(System.in);
+
+
+
+
+
+
+
+
+		public static void main(){
+			int check=0;
+			System.out.println("'1': Register(official use only)) \n'2': Query Land(Input land code)");
+			int resp = in.nextInt();
+			if (resp==1) {
+              				
+			while(check !=1){
+				System.out.println ("Welcome official!");
+				
+	         
+
+			}
+          
+		}
+     
+
+
+}
+  
+
+
+}
